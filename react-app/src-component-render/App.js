b6c7318ceeb841f2df34fef4ee4be9f26df12e5b
Tuan Tran <2036557@FITNESSINTL.COM>
2017-10-24 16:30:50 -0700

    First React component

---
+++ react-app/src-component-render/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component
+{
+  render(){
+    return (
+      <div>
+       <h1> Hello world</h1>
+       <h2> Hello world2</h2>
+       </div>
+    )
+  }
+}
+
+// const App1 = () => <h1> Hello world </h1>
+// const App = () => (
+// <div> 
+//   <h1> Hello world </h1> 
+//   <h2> Hello world 2</h2>
+// </div>)
+export default App;
