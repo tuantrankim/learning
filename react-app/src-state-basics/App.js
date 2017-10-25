2820a028ab82933e962256e1c9f1680a1ef9c50f
Tuan Tran <2036557@FITNESSINTL.COM>
2017-10-24 17:38:37 -0700

    state basics

---
+++ react-app/src-state-basics/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class App extends Component
+{
+  constructor(){
+    super();
+    this.state = {
+      txt: 'this is the state txt',
+      cat: 0
+    }    
+  }
+
+  update(e){
+    this.setState({txt: e.target.value})
+  }
+  render(){
+    return (
+      <div>
+        <input type="text" onChange={this.update.bind(this)}/>
+       <h1> {this.state.txt} - {this.state.cat}</h1>
+       </div>
+    )
+  }
+}
+
+App.propTypes = {
+  txt: PropTypes.string,
+  cat: PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+  txt: "this is the default txt"
+}
+
+export default App;
---
+++ react-app/src-state-basics/README.md
@@ -0,0 +1,6 @@
+### Source code from this lesson
+https://egghead.io/lessons/react-state-basics
+
+calling a fuction
+with "this" means calling from current instance of the class
+without this means calling from application (does not relate to this the class)
