155018c371d8482314349e386b91f0682f24983a
Tuan Tran <2036557@FITNESSINTL.COM>
2017-11-01 18:32:21 -0700

    Compose React Component Behavior with Higher Order Components

---
+++ react-app/src-fundamentals-higher-order-components-replaces-mixins/App.js
@@ -0,0 +1,53 @@
+import React from 'react'
+
+const HOC = (InnerComponent) => class extends React.Component {
+  constructor(){
+    super();
+    this.state = {count:0};
+  }
+  componentWillMount(){
+    console.log('will mount')
+  }
+  update(){
+    this.setState({count:this.state.count + 1})
+  }
+  render(){
+    return(
+      <InnerComponent
+        {...this.props}
+        {...this.state}
+        update = {this.update.bind(this)}
+      />
+    )
+  }
+}
+
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+        <ButtonHOC> button </ButtonHOC>
+        <hr/>
+        <LabelHOC> label </LabelHOC>
+      </div>
+    )
+  }
+}
+
+const ButtonHOC = HOC(
+  (props) => <button onClick={props.update}>{props.children} - {props.count} </button>
+)
+
+class Label extends React.Component{
+  componentWillMount(){
+    console.log('Label will mount')
+  }
+  render(){
+    return <label onMouseMove={this.props.update}>{this.props.children} - {this.props.count}</label>
+  }
+}
+
+const LabelHOC = HOC(Label)
+
+export default App
+
---
+++ react-app/src-fundamentals-higher-order-components-replaces-mixins/README.md
@@ -0,0 +1,8 @@
+### Source code from this lesson
+https://egghead.io/lessons/react-react-fundamentals-higher-order-components-replaces-mixins
+
+The function of higher order component is to take in acomponent and return a new component
+
+
+
+
