75d1782e468c3a28656e28a8c1590352607ffc68
tuantrankim <tuantrankim@users.noreply.github.com>
2020-01-07 17:17:43 -0800

    Create flask_basic1.py

---
+++ python-restapi/flask_basic1.py
@@ -0,0 +1,14 @@
+'''
+>pip install Flask
+>pip install Flask-RESTful
+'''
+
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return jsonify({"about": "Hello World!"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
